e08e165be9587c863a54f96bd3cd2a9e1a4e0d01
jgr68 <john.ryan@rutgers.edu>
2014-10-27 15:22:19 -0400

    Added dn as keyword argument to authenticate(), search_params as keyword argument to __init__. netid is now a keyword argument in authenticate(). Addresses #2, #3

--- ScarletLDAP3/__init__.py
+++ ScarletLDAP3/__init__.py
@@ -4,41 +4,61 @@ from ldap3 import (Server, Connection,
 class ScarletLDAP:
 
     def __init__(self, server, search_base, port,
-             user_srv = ['eden', 'pegasus', 'clam'], admin_srv = ['rci']):
+                    user_srv = ['eden', 'pegasus', 'clam'], 
+                        admin_srv = ['rci'], search_params=None):
 
         self.server    = Server(host=server, port=port, use_ssl=True)
         self.base      = search_base
         self.user_srv  = user_srv
         self.admin_srv = admin_srv
 
-    def authenticate(self, netid, passwd, use_enigma=False):
+        # set default search parameters
+        self.search_params = {
+            'filter' : '(uid=%s)',
+            'scope'  : SEARCH_SCOPE_WHOLE_SUBTREE,
+            'base'   : search_base
+        }
+
+        if search_params is not None:
+
+            # update self.search_params from valid keys search_params.keys()
+            self.search_params.update(
+                { key:val for key,val in search_params.items() 
+                  if key in self.search_params.keys() }
+            )
         
-        # establish initial connection to ldap server and bind anonymously
-        conn = Connection(self.server, user=netid, auto_bind=True)
+    def authenticate(self, passwd, netid=None, use_enigma=False, dn=None):
+        
+        if dn is None:
+
+            if netid is None:
+                raise Exception("Must specify either netid or dn as keyword arg")
+                
+            # establish initial connection to ldap server and bind anonymously
+            conn = Connection(self.server, user=netid, auto_bind=True)
        
-        # search for dn to authenticate against
-        conn.search(
-                search_base   = self.base,
-                search_scope  = SEARCH_SCOPE_WHOLE_SUBTREE,
-                search_filter = '(uid='+netid+')',
-                attributes    = ['dn']
-        )
+            # search for dn to authenticate against
+            conn.search(
+                    search_base   = self.search_params['base'],
+                    search_scope  = self.search_params['scope'],
+                    search_filter = self.search_params['filter'] % ( netid ),
+                    attributes    = ['dn']
+            )
 
-        # the user may have an account in any of the servers in server_names
-        server_names = self.admin_srv if use_enigma else self.user_srv
+            # the user may have an account in any of the servers in server_names
+            server_names = self.admin_srv if use_enigma else self.user_srv
 
-        dn = None
-        for resp in conn.response:
+            for resp in conn.response:
 
-            # check if dn is associated with a valid account in server_names
-            if len( [ s for s in server_names if s in resp['dn'] ] ):
-                dn = resp['dn']
-                break
+                # check if dn is associated with a valid account in server_names
+                if len( [ s for s in server_names if s in resp['dn'] ] ):
+                    dn = resp['dn']
+                    break
 
-        if dn == None:
-            return False
+            if dn is None:
+                return False
 
-        conn.unbind()
+            conn.unbind()
     
         # attempt to authenticate against dn using passwd
         try:
